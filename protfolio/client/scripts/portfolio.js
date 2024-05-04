617668727314e524f8c9332132ea08265ec1e5ad
Gavaskar <gavaskaraeg@gmail.com>
2024-05-04 10:55:23 +0000

    btns ready to showcase projects

--- protfolio/client/scripts/portfolio.js
+++ protfolio/client/scripts/portfolio.js
@@ -31,3 +31,72 @@ window.addEventListener('scroll', function() {
     }
   });
   
+ const projectDisplay = document.querySelector('.project-display');
+ const liElements = document.querySelectorAll('.project-categroy li');
+ 
+ liElements.forEach(li => {
+  // Add onclick event listener
+  li.addEventListener('click', () => {
+    liElements.forEach(otherLi => {
+      if (otherLi !== li) {
+          otherLi.classList.remove('selected-btn');
+      }
+  });
+
+  // Add 'selected-btn' class to the clicked <li> element
+  li.classList.add('selected-btn')
+      projectCall(li.textContent); // Log the text content of the clicked <li> element
+  });
+});
+const projectCall = (stack)=>{
+  if(stack === 'Frontend'){
+    updateFrontendOnDisplay();
+  }
+  if(stack === 'Backend'){
+    updateBackendOnDisplay();
+  }
+  if(stack === 'Full Stack'){
+    updateFSDOnDisplay();
+  }
+  if(stack === 'Game'){
+    updateGameOnDisplay();
+  }
+  if(stack === 'Freelance'){
+    updateFreelanceOnDisplay();
+  }
+}
+const updateBackendOnDisplay = ()=>{
+  projectDisplay.innerHTML = '';
+
+let div = document.createElement('div');
+div.textContent = "backend";
+projectDisplay.append(div);
+}
+const updateFrontendOnDisplay = ()=>{
+  projectDisplay.innerHTML = '';
+  let div = document.createElement('div');
+  div.textContent = "frontend";
+  projectDisplay.append(div);
+
+}
+const updateFreelanceOnDisplay = ()=>{
+  projectDisplay.innerHTML = '';
+  let div = document.createElement('div');
+  div.textContent = "freelance";
+  projectDisplay.append(div);
+
+}
+const updateGameOnDisplay = () =>{
+  projectDisplay.innerHTML = '';
+  let div = document.createElement('div');
+  div.textContent = "game";
+  projectDisplay.append(div);
+
+}
+const updateFSDOnDisplay = () =>{
+  projectDisplay.innerHTML = '';
+  let div = document.createElement('div');
+  div.textContent = "fsd";
+  projectDisplay.append(div);
+
+}
